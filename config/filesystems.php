26e27f6aed56df4c6e1f3f73eabfeb1460cdf5df
Nesma Elsafty <nesmaelsaftysm@gmail.com>
2025-04-07 10:43:44 +0200

    r2 config

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,16 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+        
+        'r2' => [
+        'driver' => 's3',
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'auto'),
+        'bucket' => env('AWS_BUCKET'),
+        'endpoint' => env('AWS_ENDPOINT'),
+        'use_path_style_endpoint' => true,
+        ],
 
     ],
 
